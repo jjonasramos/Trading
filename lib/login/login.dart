fb108ffd57b1ee9e47230f2f99b146d04fdbf09f
Jonas Ramos <jjonasramos@hotmail.com>
2019-08-03 02:44:58 -0300

    Initial commit

---
+++ lib/login/login.dart
@@ -0,0 +1,243 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  bool _isHidden = true;
+
+  void _toggleVisibility() {
+    setState(() {
+     _isHidden = !_isHidden; 
+    });
+  }
+
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIOverlays([]); // Colocar em tela cheia
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column( // Column porque os objetos estão organizados em coluna
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width, // ocupar o tamanho da tela em largura
+                height: MediaQuery.of(context).size.height/2.5,
+
+                decoration: BoxDecoration( // estilizar o container
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF43a047),
+                      Color(0xFF8bc34a),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 5,
+                      offset: Offset(0, 5),
+                    ),
+                  ],
+                ),
+
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: <Widget>[
+                    //Spacer(),
+
+                    Align(
+                      alignment: Alignment.center,
+                      child: Icon(Icons.person,
+                        size: 90,
+                        color: Colors.white,
+                      ),
+                    ),
+
+                    //Spacer(),
+
+                    Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 32,
+                          top: 32
+                        ),
+                        child: Text('Trading'.toUpperCase(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontFamily: 'Fredoka One',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+
+                ),
+              ),
+
+              Container(
+                height: MediaQuery.of(context).size.height/2,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.only(
+                  top: 62,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width/1.2,
+                      height: 50,
+                      padding: EdgeInsets.only(
+                        top: 4, 
+                        left: 16, 
+                        right: 16, 
+                        bottom: 4
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
+                        ),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          icon: Icon(Icons.email,
+                            color: Colors.grey,
+                          ),
+                          hintText: 'E-mail'
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      width: MediaQuery.of(context).size.width/1.2,
+                      height: 50,
+                      margin: EdgeInsets.only(
+                        top: 32
+                      ),
+                      padding: EdgeInsets.only(
+                        top: 4, 
+                        left: 16, 
+                        right: 16, 
+                        bottom: 4
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
+                        ),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          icon: Icon(Icons.vpn_key,
+                            color: Colors.grey,
+                          ),
+                          hintText: hintText,
+                          suffixIcon: IconButton(
+                            onPressed: _toggleVisibility,
+                            icon: Icon(Icons.visibility_off),
+                          ),
+                        ),
+                        
+                      ),
+                    ),
+
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 26,
+                          right: 32
+                        ),
+                        child: Text('Esqueceu sua senha?',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Spacer(),
+
+                    Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width/1.2,
+
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xFF43a047),
+                            Color(0xFF8bc34a),
+                          ],
+                        ),
+
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
+                        ),
+
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 5,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+
+                      child: Center(
+                        child: Text('Entrar'.toUpperCase(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:trading/login/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trading',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Login(),
+    );
+  }
+}
